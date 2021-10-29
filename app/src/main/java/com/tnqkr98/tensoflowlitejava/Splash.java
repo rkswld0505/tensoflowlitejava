c8c43668add58ec0da2d21332c08d00a0746a82f
Gwang <kancho__@naver.com>
2021-10-29 18:47:45 +0900

    Splash Activity 추가!

--- app/src/main/java/com/tnqkr98/tensoflowlitejava/Splash.java
+++ app/src/main/java/com/tnqkr98/tensoflowlitejava/Splash.java
@@ -11,7 +11,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        moveMain(1);	//1초 후 main activity 로 넘어감
+        moveMain(1);    //1초 후 main activity 로 넘어감
     }
 
     private void moveMain(int sec) {
